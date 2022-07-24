e83de00c7a7048075af288f614a24fd99b98e539
Görkem <gorkem_yelken@hotmail.com>
2022-07-24 14:29:26 +0300

    Threadlerin Yarışı

---
+++ threadlerinyarisi/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ExecutorService execute = Executors.newFixedThreadPool(4);
+
+        ArrayList<Integer> array = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            array.add(i);
+        }
+
+        ThreadYarisi thr = new ThreadYarisi(array);
+
+        execute.execute(thr);
+
+
+
+    }
+}
